480988135c5483b8e18a3e7a0f5146b6e8b51fa4
Tom Sanderson <tom.w.j.sanderson@gmail.com>
2020-01-03 10:36:56 -0500

    first commit

---
+++ src/App/app.container.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <section>
+      <h1>Nav</h1>
+        <h1>Home</h1>
+        <h1>About Me</h1>
+        <h1>My Work</h1>
+        <h1>Tutorials</h1>
+        <h1>Contact</h1>
+      <h1>Footer</h1>
+    </section>
+  );
+}
+
+export default App;
