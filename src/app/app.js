6bb858722c711281453998271a50091b5401964f
Tom Sanderson <tom.w.j.sanderson@gmail.com>
2020-03-08 21:56:14 -0400

    updated readme

--- src/app/app.js
+++ src/app/app.js
@@ -12,7 +12,7 @@ import Writing from '../pages/writing';
 import Contact from '../pages/contact';
 import Footer from '../ui/footer';
 
-// TODo: add tests, write readme, add icons to make it progressive web app, manual QA, check on different browser, deploy to firebase
+// TODo: add icons to make it progressive web app, manual QA, check on different browser, deploy to firebase
 
 const App = () => {
   return (
